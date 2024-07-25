804ce8feb881586cd269dcd324000a78367b0862
raul <raulmay8@hotmail.com>
2024-07-25 10:23:11 -0500

    corrección de importación en el page crudespecialidad

--- src/Pages/Public/Jorge/CrudEspecialidad.tsx
+++ src/Pages/Public/Jorge/CrudEspecialidad.tsx
@@ -1,6 +1,6 @@
 
 import { Box } from "@mui/material";
-import { EspecialidadPage } from "c:/Users/angel/Pictures/front/GestionClinicas/src/Components/Public/Jorge/EspecialidadPage"
+import { EspecialidadPage } from "../../../Components/Public/Jorge/EspecialidadPage";
 
 
 export const CrudEspecialidad = () => {
