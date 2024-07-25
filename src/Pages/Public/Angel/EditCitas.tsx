c436cc849f11b9a70b0aef0515b1d0a503a9a996
angelarias1 <106706081+angelarias1@users.noreply.github.com>
2024-07-25 18:46:16 -0500

    editar vistas

---
+++ src/Pages/Public/Angel/EditCitas.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material"
+import EditarCitas from "../../../Components/Public/Angel/EditarCitas"
+
+
+export default function EditCitas() {
+  return (
+
+    <Box>
+        <EditarCitas/>
+    </Box>
+
+)
+}
