b2e78a5d66240619438a7d36b0e7c10fb7295ee1
angelarias1 <106706081+angelarias1@users.noreply.github.com>
2024-07-25 04:18:29 -0500

    tabla horarios

--- src/Pages/Public/Jorge/CrudEspecialidad.tsx
+++ src/Pages/Public/Jorge/CrudEspecialidad.tsx
@@ -1,7 +1,6 @@
 
 import { Box } from "@mui/material";
-import EspecialidadPage from "../../../Components/Public/Jorge/EspecialidadPage";
-
+import { EspecialidadPage } from "c:/Users/angel/Pictures/front/GestionClinicas/src/Components/Public/Jorge/EspecialidadPage"
 
 
 export const CrudEspecialidad = () => {
